a76d71664c3830d58878ff61936621c6a858125f
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-28 19:06:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+var musicians = ['JohnLennon', 
+'PaulMcCarthy', 
+'GeaorgeHarris',
+'RingoStarr'];
+
+var instruments = ['Guitar',
+'Bass Guitar',
+'Lead Guitar',
+'Drums']; 
+
+
+
+function theBeatlesPlay ( musicians, plays, instruments) { console.log ('musicians plays instruments'); 
+} 
+  
+  
